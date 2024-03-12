fe1b1faac6d217ac3904432f2c4c2c8dfcfff3c4
Luan <luan.emanuelriar@gmail.com>
2024-03-12 16:00:00 -0300

    Começando do zero.
    Co-authored-by: GDuzzi <GDuzzi@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -1,78 +1,85 @@
+/*
+Integrantes do grupo:
+Gustavo Paiva                   RA:
+Gustavo Teixeira                RA:
+Luan E. Rinaldi                 RA: 2184611 
+Pedro Dezem                     RA:
+*/  
+
 #include <iostream>
 #include <random>
 
 using namespace std;
 
-char* toBinary(int n, int tamanho) {
-  static char r[100] = {0};
-  std::fill_n(r, tamanho, '0');
-  int i = tamanho - 1;
-  while (n != 0 && i >= 0) {
-        r[i] = (n % 2 == 0 ? '0' : '1');
-        i--;
-        n /= 2;
-  }
-  return r;
+struct Populacao{
+    static const unsigned n_individuos = 10;
+    int individuos[n_individuos];
+};
+
+Populacao cria_populacao(int min, int max){
+    Populacao Populacao_1;
+    for(int i = 0; i < Populacao_1.n_individuos; i++){
+        Populacao_1.individuos[i] = min + (rand() % (max - min + 1));
+    }
+    return Populacao_1;
 }
 
+// 1 = identificar o bit dentro do int
+// 2 = mudar o bit dentro do int
 
-// string toBinary(int n, int tamanho) {
-//   string r = "";
-//   while (n != 0) {
-//         r.append = (n % 2 == 0 ? '0' : '1');
-//         n /= 2;
-//   }
-//   return r;
-// }
+// por enquanto é isso?
+// ss
+
+
+int main(){
+    srand(time(NULL));
+    float crossover = 0 , mutacao = 0;
+    unsigned max_geracoes = 1 , rand_min= 0 , rand_max = 0;
+    cout << "Qual será a taxa de crossover (0 -> 90%)? : ";
+    cin >> crossover;
+    cout << "Qual será a taxa de mutação (0 -> 90%)? : ";
+    cin >> mutacao;
+    cout << "Quantas gerações serão analizadas? : ";
+    cin >> max_geracoes;
+    cout << "Qual será o valor mínimo dos números gerados? : ";
+    cin >> rand_min;
+    cout << "Qual será o valor máximo dos números gerados? :";
+    cin >> rand_max;
+
+    cout << endl;
+    cout << "Taxa de crossover escolhida: " << crossover << " %" << endl;
+    cout << "Taxa de mutação escolhida: " << mutacao << " %" << endl;
+    cout << "Número máximo de gerações : " << max_geracoes << endl;
+    crossover = crossover / 100;
+    mutacao = mutacao / 100;
+
+    Populacao teste = cria_populacao(rand_min,rand_max);
+    cout << "População Inicial: " << endl;
+    for(int i = 0 ; i < teste.n_individuos ; i++){
+        cout << teste.individuos[i] << " | ";
+    }
+    cout << endl;
+    for(int i = 0 ; i < max_geracoes ; i++){
+        // avaliar população
+        if(){
+            // armazenar o melhor individuo ate o momento
+            //fórmula p/ comparação = a*pow(n,5)+b*pow(n,4)+c*pow(n,3)+d*pow(n,2)+e*n+f = 0 , a != 0
+        }
+        
+        // verificar se a solução está aceitável (se sim, encerrar laço)
+        // selecionar os melhores indivíduos
+        // aplicar cruzamento com crossover
+        // aplicar mutação
+    }
+
+}
 
 // tamanho população
 // n de gerações
 // taxa de mutação
-// intervalo minimo e max de numero aleatório (limitante da população inicial)
+// intervalo minimo e max de numero aleatório (limitante da população inicial) <-----
 // inserir valores de a,b,c,d,e,f p/ equação de quinto grau (p/ futura comparação)
 // tamanho do indivíduo = tamanho de um int = 4bytes = 32 bits
 // chance de um bit alterar seu valor
 // altera apenas 1 bit em cada indivíduo por vez (bit sorteado)
 // objetivo é chegar o mais próximo possível da raiz de uma função de 5º grau
-
-int main(){
-        srand(time(NULL)); // Cria seed para geração de numeros aleatórios com base  
-        const int tamanho_populacao = 10;
-        float crossover = 0 , mutacao = 0;
-        int geracoes = 1;
-        cout << "Qual será a taxa de crossover (0 -> 90%)? : ";
-        cin >> crossover;
-        cout << "Qual será a taxa de mutação (0 -> 90%)? : ";
-        cin >> mutacao;
-        cout << "Quantas gerações serão analizadas? : ";
-        cin >> geracoes;
-
-        cout << endl;
-        for (int i = 0; i < 3; i++)
-        {
-            cout << "Tamanho da população "<< i <<" = " << tamanho_populacao << endl; 
-        }
-        cout << "Taxa de crossover escolhida: " << crossover << " %" << endl;
-        cout << "Taxa de mutação escolhida: " << mutacao << " %" << endl;
-        cout << "Número máximo de gerações : " << geracoes << endl;
-
-        crossover = crossover / 100;
-        mutacao = mutacao / 100;
-
-        cout << endl;
-
-        const unsigned short tamanho_individuo = 32;
-        char* indiv_binario;
-        char populacao[tamanho_populacao][tamanho_individuo];
-        for (int i = 0; i < tamanho_populacao; i++){
-            long range = pow(2 , tamanho_individuo);
-            long individuo = rand() % range;
-            indiv_binario = toBinary(individuo, tamanho_individuo);
-            for (int j = 0 ; j < tamanho_individuo ; j++){
-                populacao[i][j] = indiv_binario[j];
-                cout << indiv_binario[j] << " ";
-            }
-            cout << "     =    " << individuo << endl;
-            cout << endl;
-        }           
-}
